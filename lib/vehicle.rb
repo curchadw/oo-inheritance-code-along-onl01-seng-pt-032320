00756905895cc5044b2c74d3fd38c7774b945c34
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-11 17:46:25 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,11 @@
 class Vehicle
   attr_accessor :wheel_size, :wheel_number
- 
+
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
- 
+
   def go
     "vrrrrrrrooom!"
   end
@@ -13,6 +13,6 @@ class Vehicle
   def fil_up_tank
     "filing up!"
   end
- 
-  
+
+
 end
